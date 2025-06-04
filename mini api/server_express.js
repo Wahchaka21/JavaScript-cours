00fba128a1a44af5f0eb826ba5be93abc02b4b86
Wahchaka21 <quentyn.petitjean@hotmail.com>
2025-06-04 16:23:57 +0200

    transformation code http en express.js

--- mini api/server_express.js
+++ mini api/server_express.js
@@ -2,44 +2,63 @@
 //sur postman
 const express = require("express");
 const fs = require("fs");
-
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // Pour pouvoir lire le JSON envoyé dans le body
 
+const filePath = "users.json";
+
+// GET - Lire tous les utilisateurs
 app.get("/users", (req, res) => {
-  const data = fs.readFileSync("users.json", "utf-8");
+  const data = fs.readFileSync(filePath, "utf-8");
   res.send(data);
 });
 
+// POST - Ajouter un utilisateur
 app.post("/users", (req, res) => {
-  const newUser = req.body;
-  const file = fs.readFileSync("users.json", "utf-8");
-  const users = JSON.parse(file);
-  users.push(newUser);
-  fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
+  const user = req.body;
+  const file = fs.readFileSync(filePath, "utf-8");
+  let users = JSON.parse(file);
+  users.push(user);
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
   res.send("Utilisateur créé");
 });
 
-app.put("/users/3", (req, res) => {
-  const updateUser = req.body;
-  const file = fs.readFileSync("users.json", "utf-8");
-  const users = JSON.parse(file);
-  const index = users.findIndex((u) => u.id === 3);
+// PUT - Modifier un utilisateur par ID
+app.put("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedUser = req.body;
+
+  const file = fs.readFileSync(filePath, "utf-8");
+  let users = JSON.parse(file);
+  const index = users.findIndex((u) => u.id === id);
 
   if (index !== -1) {
-    users[index] = updateUser;
-    fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
-    res.send("Utilisateur mis à jour !");
+    users[index] = { ...users[index], ...updatedUser };
+    fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+    res.send("Utilisateur modifié");
   } else {
     res.status(404).send("Utilisateur non trouvé");
   }
 });
 
-app.use((req, res) => {
-  res.status(404).send("Not found");
+// DELETE - Supprimer un utilisateur par ID
+app.delete("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const file = fs.readFileSync(filePath, "utf-8");
+  let users = JSON.parse(file);
+  const newUsers = users.filter((u) => u.id !== id);
+
+  if (newUsers.length === users.length) {
+    res.status(404).send("Utilisateur non trouvé");
+  } else {
+    fs.writeFileSync(filePath, JSON.stringify(newUsers, null, 2));
+    res.send("Utilisateur supprimé");
+  }
 });
 
+// Lancer le serveur
 app.listen(3000, () => {
-  console.log("Serveur Express en ligne sur http://localhost:3000/users");
+  console.log("Serveur Express lancé sur http://localhost:3000/users");
 });
