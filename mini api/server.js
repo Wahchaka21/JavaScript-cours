c2f24df08641242936b3c2514aa4fdd1df63b3cb
Wahchaka21 <quentyn.petitjean@hotmail.com>
2025-06-10 11:36:57 +0200

    modification serveur http non fini

--- mini api/server.js
+++ mini api/server.js
@@ -4,9 +4,22 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/users" && req.method === "GET") {
-    const data = fs.readFileSync("users.json", "utf-8");
-    res.end(data);
-  } else if (req.url === "/users" && req.method === "POST") {
+    const file = fs.readFileSync("users.json", "utf-8");
+    let tableauUsers = JSON.parse(file)
+
+    let html = `<table>
+                  <tr>
+                    <td>Id</td>
+                    <td>Nom</td>
+                  </tr>`
+  tableauUsers.forEach(elem => html += `<tr><td>` + elem.id + `</td><td>` + elem.nom + `</td><td>Modifier</td>` + `<td><button onclicks="supprimerUser(' + elem.id ' )">Supprimer</button></td>` + `</td></tr>` + `</td></tr>`)
+
+  html += `</table>`
+
+  res.setHeader("Content-Type", "text/html; charset=utf-8")
+  res.end(html)
+  } 
+  else if (req.url === "/users" && req.method === "POST") {
     let body = "";
 
     req.on("data", (chunk) => {
@@ -20,7 +33,8 @@ const server = http.createServer((req, res) => {
       fs.writeFileSync("users.json", JSON.stringify(tableauUsers));
       res.end("Utilisateur créé");
     });
-  } else if (req.url.substring(0, 7) === "/users/" && req.method === "PUT") {
+  } 
+  else if (req.url.substring(0, 7) === "/users/" && req.method === "PUT") {
     const tabUrl = req.url.split("/");
     const idUser = parseInt(tabUrl[2]);
     //console.log(idUser);
@@ -41,12 +55,20 @@ const server = http.createServer((req, res) => {
         fs.writeFileSync("users.json", JSON.stringify(tableauUsers));
         res.end("utilisateur modifié");
       });
-    } else {
+    } 
+    else {
       res.end("utilisateur non trouvé");
     }
-  } else if (req.url === "/users/" && req.method === "DELETE") {
-    //a voir le delete
-  } else {
+  } else if (req.url.substring(0, 7) === "/users/" && req.method === "DELETE") {
+    const tabUrl = req.url.split("/")
+    const idUser = parseInt(tabUrl[2])
+    const file = fs.readFileSync("users.json", "utf-8")
+    let tableauUsers = JSON.parse(file)
+    const nouveauTab = tableauUsers.filter((u) => u.id !== idUser)
+    fs.writeFileSync("users.json", JSON.stringify(nouveauTab))
+    res.end("Utilisateur supprimé")
+  } 
+  else {
     res.statusCode = 404;
     res.end("not found");
   }
