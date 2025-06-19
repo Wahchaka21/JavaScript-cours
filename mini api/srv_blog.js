8a5a3a445a9997459adeab173afb942dfe534b8c
Wahchaka21 <quentyn.petitjean@hotmail.com>
2025-06-19 14:55:45 +0200

    ajout middleware

--- mini api/srv_blog.js
+++ mini api/srv_blog.js
@@ -6,6 +6,13 @@ const port = 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')  
+    next()
+})
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "srv_blog.html"))
 })
